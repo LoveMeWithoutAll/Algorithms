b90d8d4c2a91438b22ba1984b20089cdd4f4f87f
lovemewithoutall <lovemewithoutall@gmail.com>
2017-02-26 15:50:41 +0900

    rotate a square matrix by 90 degrees

---
+++ src/DataStructure/rotateMatrix.java
@@ -0,0 +1,61 @@
+package DataStructure;
+
+/*
+    rotate a square matrix by 90 degrees
+ */
+
+public class rotateMatrix {
+
+    public static void main(String args[]){
+
+        //int [][] input = new int[][]{{1,2,3},{4,5,6},{7,8,9}};
+        int [][] input = new int[][]{{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+
+        printMatrix(input);
+        rotate(input);
+        printMatrix(input);
+    }
+
+    private static void printMatrix(int [][] input){
+        System.out.println();
+        for(int [] xAxis : input){
+            System.out.println();
+            for(int yAxis : xAxis) System.out.print(" " + yAxis);
+        }
+    }
+
+
+    private static boolean rotate(int [][] input){
+
+        // assertion
+        if( input.length == 0 || input.length != input[0].length ) return false;
+
+        // rotate
+        for(int layer = 0; layer < input.length/2 ; layer++){
+            int first = layer;
+            int last = input.length - 1 - layer; // init: 3-1-0=2
+            for( int i=first ; i < last ; i ++){
+
+                //i's offset: first is on move, layer is on hold
+                int offset = input.length - 1 - i; // init: 3-1-0=2 , second: 3-1-1=1
+
+                // top -> temp
+                int temp = input[first][i];
+
+                //left -> top
+                input[first][i] = input[offset][first];
+
+                // bottom -> left
+                input[offset][first] = input[last][offset];
+
+                // right => bottom
+                input[last][offset] = input[i][last];
+
+                // temp => right
+                input[i][last] = temp;
+            }
+        }
+
+        return true;
+    }
+}
