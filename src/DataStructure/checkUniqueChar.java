2ec69ad717b556ffc8b294fd34615437e5d34a51
lovemewithoutall <lovemewithoutall@gmail.com>
2017-02-26 16:29:06 +0900

    is it a set of unique chars?

---
+++ src/DataStructure/checkUniqueChar.java
@@ -0,0 +1,39 @@
+package DataStructure;
+
+/*
+    when using extended ascii code, check if it is a character set which is a set of unique characters.
+ */
+
+public class checkUniqueChar {
+
+    public static void main (String []args){
+        String[] words = {"abcde", "hello", "apple", "kite", "padle"};
+
+        for( String test : words){
+            if( checkUnique(test) ){
+                System.out.println(test + " is a set of unique chars.");
+            } else {
+                System.out.println(test + " have duplicate chars.");
+            }
+        }
+    }
+
+    private static boolean checkUnique(String input){
+        // assertion
+        if( input.length() > 256 ) return false;
+
+        boolean [] set = new boolean[256];
+
+        for( int i=0 ; i < input.length(); i++){
+            int asciiNum = input.charAt(i);
+            if(set[asciiNum]){
+                return false;
+            } else {
+                set[asciiNum] = true;
+            }
+        }
+
+        return true;
+    }
+
+}
