ac66d6e2379fdaa3da3569a12e2c9f964d26f0c6
lovemewithoutall <lovemewithoutall@gmail.com>
2017-03-18 00:32:10 +0900

    get a number of possible combinations from the same set

---
+++ src/Recursive/CombinationTest.java
@@ -0,0 +1,14 @@
+package Recursive;
+
+import java.util.LinkedList;
+
+/**
+ * Created by ys on 2017-03-17.
+ */
+public class CombinationTest {
+    public static void main(String [] args){
+        Combination combi = new Combination();
+        int result = combi.getCombination(40, 6);
+        System.out.println(result);
+    }
+}
