a0c5608dad5dafde1116d63e032bbaf0d31bbea4
lovemewithoutall <lovemewithoutall@gmail.com>
2017-03-27 23:42:26 +0900

    Check whether array A is a permutation

---
+++ src/Math/PermCheck.java
@@ -0,0 +1,68 @@
+package Math;
+
+import java.util.Arrays;
+
+/**
+ * PermCheck: Check whether array A is a permutation.
+ * Reference: Codility.com
+ * Description:
+ * A non-empty zero-indexed array A consisting of N integers is given.
+ A permutation is a sequence containing each element from 1 to N once, and only once.
+
+ For example, array A such that:
+ A[0] = 4
+ A[1] = 1
+ A[2] = 3
+ A[3] = 2
+ is a permutation, but array A such that:
+ A[0] = 4
+ A[1] = 1
+ A[2] = 3
+ is not a permutation, because value 2 is missing.
+ The goal is to check whether array A is a permutation.
+
+ Write a function:
+ class Solution { public int solution(int[] A); }
+ that, given a zero-indexed array A, returns 1 if array A is a permutation and 0 if it is not.
+
+ For example, given array A such that:
+ A[0] = 4
+ A[1] = 1
+ A[2] = 3
+ A[3] = 2
+ the function should return 1.
+ Given array A such that:
+ A[0] = 4
+ A[1] = 1
+ A[2] = 3
+ the function should return 0.
+
+ Assume that:
+ N is an integer within the range [1..100,000];
+ each element of array A is an integer within the range [1..1,000,000,000].
+
+ Complexity:
+ expected worst-case time complexity is O(N);
+ expected worst-case space complexity is O(N), beyond input storage (not counting the storage required for input arguments).
+ Elements of input arrays can be modified.
+ */
+public class PermCheck {
+
+    public static void main(String [] args){
+        System.out.println(solution(new int [] {1,2}));
+    }
+
+    // score:70%, https://codility.com/demo/results/trainingB6QUQM-WUP/
+    public static int solution(int[] A) {
+        // write your code in Java SE 8
+        int len = A.length;
+        long lenSum = (len * (len+1)) / 2;
+        long sumOfArr = Arrays.stream(A).sum();
+
+        if( lenSum == sumOfArr ) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+}
